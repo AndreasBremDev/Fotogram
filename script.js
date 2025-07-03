5740cbb1cd01d28f7452dc57a4474010b718c456
Andreas Brem <andi.brem@web.de>
2025-07-03 07:17:16 +0200

    updates script defer and script itself

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let imgsLoad = [
     'band3.png',
     'band1.png',
     'band2.png',
+    'band3.png',
+    'band1.png',
+    'band2.png',
     'band3.png'
 ];
 const imgsRef = document.getElementById('imgsRef');
@@ -16,8 +19,8 @@ function renderImages() {
     for (let i = 0; i < imgsLoad.length; i++) {
         console.log(imgsRef.innerHTML);
         imgsRef.innerHTML += renderTest(i);
-   }
+    }
 }
- function renderTest(i) {
-    return `<img src="./img/${imgsLoad[i]}" alt=picture${[i+1]} class="img-elem">`;
- }
+function renderTest(i) {
+    return `<img src="./img/${imgsLoad[i]}" alt=picture${[i + 1]} class="img-elem">`;
+}
