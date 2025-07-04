bf76a05744de39f557a6f9ac70adc5e1a4ebb41a
Andreas Brem <andi.brem@web.de>
2025-07-04 12:36:23 +0200

    changes in pictures sizes

--- script.js
+++ script.js
@@ -3,47 +3,97 @@ let imgsLoad = [
     'band1.png',
     'band2.png',
     'band3.png'
-    // 'band1.png',
-    // 'band2.png',
-    // 'band3.png',
-    // 'band1.png',
-    // 'band2.png',
-    // 'band3.png',
-    // 'band1.png',
-    // 'band2.png',
-    // 'band3.png',
-    // 'band1.png',
-    // 'band2.png',
-    // 'band3.png'
 ];
+
+let imagesFotogram = [
+    {
+        filename: '01.webp',
+        title: 'Schloss Burghausen',
+        location: 'Burghausen'
+    },
+    {
+        filename: '02.webp',
+        title: 'Alpen Aussicht',
+        location: 'Ostersehen'
+    },
+    {
+        filename: '03.webp',
+        title: 'Karlsfelder See',
+        location: 'bei München'
+    },
+    {
+        filename: '04.webp',
+        title: 'Aussicht auf Bad Tölz',
+        location: 'Bad Tölz'
+    },
+    {
+        filename: '05.webp',
+        title: 'krorriger Baum',
+        location: 'Schlosspark Nymphenburg'
+    },
+    {
+        filename: '06.webp',
+        title: 'Palmen in Sahl Hasheesh',
+        location: 'Ägypten'
+    },
+    {
+        filename: '07.webp',
+        title: 'Palmen in Sahl Hasheesh',
+        location: 'Ägypten'
+    },
+    {
+        filename: '08.webp',
+        title: 'Fontaine',
+        location: 'Olympiapark'
+    },
+    {
+        filename: '09.webp',
+        title: 'Olympiapark',
+        location: 'München'
+    },
+    {
+        filename: '10.webp',
+        title: 'Amper',
+        location: 'bei München'
+    },
+        {
+        filename: '11.webp',
+        title: 'Feldmochinger See',
+        location: 'bei München'
+    }
+]
+
 const imgSmallRef = document.getElementById('imgSmallRef');
 const overlayRef = document.getElementById('overlay');
 let myInterval;
 
 function renderImagesSmall() {
-    for (let i = 0; i < imgsLoad.length; i++) {
-        imgSmallRef.innerHTML += renderSmallImages(i, imgsLoad);
+    for (let i = 0; i < imagesFotogram.length; i++) {
+        imgSmallRef.innerHTML += renderSmallImages(i, imagesFotogram);
     }
 };
 
-function renderSmallImages(i, imgsLoad) {
+function renderSmallImages(i, imagesFotogram) {
     return `
     <span><a>
-    <img onclick="overlayOn(${i})" src="./img/${imgsLoad[i]}" alt=picture${[i + 1]} class="img-small">
+    <img onclick="overlayOn(${i})" src="./img/${imagesFotogram[i].filename}" alt=picture${[i + 1]} class="img-small">
     </a></span>`;
 };
 
-function overlayOn(i){
+
+// ONLY UNTIL UP HERE
+
+function overlayOn(i) {
     overlayRef.style.display = "block";
-    if (i > imgsLoad.length - 1) {
+    if (i > imagesFotogram.length - 1) {
         i = 0;
     } else if (i < 0) {
-        i = imgsLoad.length -1;
+        i = imagesFotogram.length - 1;
     }
-    overlayRef.innerHTML = createOverlayContent(i, imgsLoad);
+    overlayRef.innerHTML = createOverlayContent(i, imagesFotogram);
 };
 
-function createOverlayContent(i, imgsLoad) {
+function createOverlayContent(i, imagesFotogram) {
     return `
     <div onclick="stopBubbling(event)" id="overlayImgContainer" class="overlay-img-container">
         <div class="overlayTop">
@@ -52,40 +102,40 @@ function createOverlayContent(i, imgsLoad) {
                 <img id="playPause" onclick="diashowStop(), stopBubbling(event)" src="./img/icons/pause-solid.svg">
                 Diashow
             </button> 
-            <p>${imgsLoad[i].slice(0,-4)}</p>
+            <p>${imagesFotogram[i].title} - ${imagesFotogram[i].location}</p>
             <button onclick="overlayOff()" class="btn-close">X</button>
         </div>
-        <img src="./img/${imgsLoad[i]}" alt=picture${[i + 1]} class="img-overlay">
+        <img src="./img/${imagesFotogram[i]}" alt=picture${[i + 1]} class="img-overlay">
         <div class="overlayBottom">
             <button onclick="overlayOn(${i - 1}), diashowStop()" class="btn-prev">&lt;</button>
-            <p>${i +1} / ${imgsLoad.length}</p>
+            <p>${i + 1} / ${imagesFotogram.length}</p>
             <button onclick="overlayOn(${i + 1}), diashowStop()" class="btn-next">&gt;</button>
         </div>
     </div>
     `;
 }
 
-function overlayOff(){
+function overlayOff() {
     overlayRef.style.display = "none";
     diashowStop();
 }
 
-function stopBubbling(event){
+function stopBubbling(event) {
     event.stopPropagation();
 }
 
 
-function diashow(i){
+function diashow(i) {
     document.getElementById('playPause').src = "./img/icons/play-solid.svg";
     myInterval = setInterval(() => {
         i += 1;
-        if (i > imgsLoad.length - 1) {
+        if (i > imagesFotogram.length - 1) {
             i = 0;
         }
         overlayOn(i);
         document.getElementById('playPause').src = "./img/icons/play-solid.svg";
     }, 2500);
-    
+
 }
 
 function diashowStop() {
